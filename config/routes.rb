aec9b74621fa512bd1bfa018ce45245d0f73e74a
Brad Lindsay <sluggy.fan@gmail.com>
2022-03-21 22:29:53 -0500

    Create routes for scrapbook pages
    The routes are set up so that the following will work:
    
    The first scrapbook in the list:
    `/same/as/files/under/pages`
    `/pages`
    `/pages/new`
    `/pages/same/as/files/under/pages`
    `/pages/same/as/files/under/pages/edit`
    
    If multiple scrapbooks:
    `/:book/same/as/files/under/pages`
    `/:book/pages`
    `/:book/pages/new`
    `/:book/pages/same/as/files/under/pages`
    `/:book/pages/same/as/files/under/pages/edit`
    
    The idea is that full URLs will start with the basename of the book and
    the literal word "pages" as the path prefix, but we also allow for some
    short versions of the path when viewing pages. (All of them are handled
    by 'pages#show').

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 Scrapbook::Engine.routes.draw do
+  book_regex = /#{Scrapbook::Engine.config.scrapbook.paths.map { File.basename(_1) }.join('|')}/
+
   resources :pages, id: /.+/
+
+  resources :pages, path: ':book/pages', id: /.+/, constraints: {book: book_regex}
+
+  get ':book/*id', to: 'pages#show', constraints: {book: book_regex}
+  get '*id', to: 'pages#show'
 end
---
+++ spec/routing/scrapbook_routes_spec.rb
@@ -0,0 +1,126 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Scrapbook do
+  routes { Scrapbook::Engine.routes }
+
+  describe 'Direct routes to pages' do
+    context 'without the pages prefix' do
+      it 'correctly routes viewing a long path' do
+        expect(get: '/area/subject/item').to route_to(
+          controller: 'scrapbook/pages',
+          action: 'show',
+          id: 'area/subject/item'
+        )
+      end
+    end
+
+    context 'with the pages prefix' do
+      it 'correctly routes indexing the pages' do
+        expect(get: '/pages').to route_to('scrapbook/pages#index')
+      end
+
+      it 'correctly routes viewing a long path' do
+        expect(get: '/pages/area/subject/item').to route_to(
+          controller: 'scrapbook/pages',
+          action: 'show',
+          id: 'area/subject/item'
+        )
+      end
+
+      it 'correctly routes editing a long path' do
+        expect(get: '/pages/area/subject/item/edit').to route_to(
+          controller: 'scrapbook/pages',
+          action: 'edit',
+          id: 'area/subject/item'
+        )
+      end
+
+      it 'correctly routes updating a long path' do
+        expect(put: '/pages/area/subject/item').to route_to(
+          controller: 'scrapbook/pages',
+          action: 'update',
+          id: 'area/subject/item'
+        )
+      end
+
+      it 'correctly routes the new page path' do
+        expect(get: '/pages/new').to route_to('scrapbook/pages#new')
+      end
+
+      it 'correctly routes creating a new page' do
+        expect(post: '/pages').to route_to('scrapbook/pages#create')
+      end
+    end
+  end
+
+  # Since the default book is "scrapbook", that's the prefix we will use. This
+  # means that mounted under "scrapbook" in an application, the URLs would be
+  # in the form "/scrapbook/scrapbook/..."
+  describe 'Book-prefixed routes to pages' do
+    context 'without the pages prefix' do
+      it 'correctly routes viewing a long path' do
+        expect(get: '/scrapbook/area/subject/item').to route_to(
+          controller: 'scrapbook/pages',
+          action: 'show',
+          book: 'scrapbook',
+          id: 'area/subject/item'
+        )
+      end
+    end
+
+    context 'with the pages prefix' do
+      it 'correctly routes indexing the pages' do
+        expect(get: '/scrapbook/pages').to route_to(
+          controller: 'scrapbook/pages',
+          action: 'index',
+          book: 'scrapbook'
+        )
+      end
+
+      it 'correctly routes viewing a long path' do
+        expect(get: '/scrapbook/pages/area/subject/item').to route_to(
+          controller: 'scrapbook/pages',
+          action: 'show',
+          book: 'scrapbook',
+          id: 'area/subject/item'
+        )
+      end
+
+      it 'correctly routes editing a long path' do
+        expect(get: '/scrapbook/pages/area/subject/item/edit').to route_to(
+          controller: 'scrapbook/pages',
+          action: 'edit',
+          book: 'scrapbook',
+          id: 'area/subject/item'
+        )
+      end
+
+      it 'correctly routes updating a long path' do
+        expect(put: '/scrapbook/pages/area/subject/item').to route_to(
+          controller: 'scrapbook/pages',
+          action: 'update',
+          book: 'scrapbook',
+          id: 'area/subject/item'
+        )
+      end
+
+      it 'correctly routes the new page path' do
+        expect(get: '/scrapbook/pages/new').to route_to(
+          controller: 'scrapbook/pages',
+          action: 'new',
+          book: 'scrapbook'
+        )
+      end
+
+      it 'correctly routes creating a new page' do
+        expect(post: '/scrapbook/pages').to route_to(
+          controller: 'scrapbook/pages',
+          action: 'create',
+          book: 'scrapbook'
+        )
+      end
+    end
+  end
+end
