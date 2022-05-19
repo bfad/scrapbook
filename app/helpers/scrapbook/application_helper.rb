97bf2a07b6d427859c4ab30b22bafa90e4e936dd
Brad Lindsay <sluggy.fan@gmail.com>
2022-05-19 15:59:50 -0500

    Reflect updates to main app helper methods in Scrapbook
    When `Scrapbook::ApplicationHelper` was reloaded, it didn't pick up
    changes made to the main app's helpers. I think this was do to Rails's
    memoization of the helpers, and this works around that.

--- app/helpers/scrapbook/application_helper.rb
+++ app/helpers/scrapbook/application_helper.rb
@@ -3,6 +3,12 @@
 module Scrapbook
   # Engine-wide helpers
   module ApplicationHelper
-    include Rails.application.helpers
+    # Using `include Rails.application.helpers` didn't work for reloading changes
+    # made to the main app's helper methods, but this is adapted from its code.
+    # (I think it's getting around the memoized "@helpers" variable.)
+    # https://github.com/rails/rails/blob/6bfc637659248df5d6719a86d2981b52662d9b50/railties/lib/rails/engine.rb#L494
+    ActionController::Base.modules_for_helpers(
+      ActionController::Base.all_helpers_from_path(Rails.application.helpers_paths)
+    ).each { include _1 }
   end
 end
